e064c93f8c017692865dff16415f74d88105bac4
shireenh123 <shireenh123@gmail.com>
2026-09-12 20:35:06 +0530

    Update app.py

--- frontend/app.py
+++ frontend/app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import requests
 
 # Base URL of the Flask backend
-BACKEND_URL = "http://salesbackend:7860"
+BACKEND_URL = "http://backend:7860"
 
 # Set the title of the Streamlit app
 st.title("SuperKart Sales Prediction")
